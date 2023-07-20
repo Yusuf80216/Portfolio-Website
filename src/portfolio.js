e080ad384c8950343bd13d293f54480ede9dc8c4
Yusuf Ansari <myusuf20comp@student.mes.ac.in>
2023-07-20 22:46:27 +0530

    updated resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "Machine Learning Enthusiast | Aspiring Data Scientist"
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1tekumzrZ3S8omrudx35RrXVNDoJC8Dpk/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1896IIQIndo5fkPS6jU7mC6D1rrsNEeis/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
